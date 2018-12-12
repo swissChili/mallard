18752dd45831a61607a224dfa94a673297a2016b
swissChili <sasha@fastmail.com>
2018-12-12 09:41:15 -0800

    Fixed broken tests

--- spec/mallard_spec.cr
+++ spec/mallard_spec.cr
@@ -3,11 +3,11 @@ require "./spec_helper"
 describe Mallard do
 
     it "lex" do
-        Mallard::Lexer.new.tokenize("let foo ->\n    bar\nfoo qux"))
+        Mallard::Lexer.new.tokenize("let foo ->\n    bar\nfoo qux")
     end
 
     it "parse" do
-        Mallard::Parrser.new.parse(Mallard::Lexer.new.tokenize("let foo ->\n    bar\nfoo qux")))
+        Mallard::Parrser.new.parse(Mallard::Lexer.new.tokenize("let foo ->\n    bar\nfoo qux"))
     end
 
 end
